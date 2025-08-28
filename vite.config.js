7175cb58ea1a81e6d38fef8d3c45b56b34993595
Júlia Gonçalves <90739280+juliagoncaalves@users.noreply.github.com>
2025-08-28 20:48:56 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/to-do-list-react/', // 👈 coloque o NOME exato do repositório do GitHub aqui
+  base: '/to-do-list-react/',
 });
